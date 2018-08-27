4ced088301be49af2d739a477439960b8ec7a2dc
kaikai <m13339490373@163.com>
2018-08-27 14:50:56 +0800

    第一次提交

---
+++ js/ggad2_728x90.js
@@ -0,0 +1,12 @@
+document.writeln("<link type=\"text/css\" rel=\"stylesheet\" href=\"http://www.cssmoban.com/statics/css/astyle.css\" />");
+document.writeln("<div align=\"center\">");
+document.writeln("<script async src=\'//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script>");
+document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:block\'");
+document.writeln("     data-ad-client=\'ca-pub-1542822386688301\'");
+document.writeln("     data-ad-slot=\'5966457456\'");
+document.writeln("     data-ad-format=\'auto\'></ins>");
+document.writeln("<script>");
+document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
+document.writeln("</div>");
